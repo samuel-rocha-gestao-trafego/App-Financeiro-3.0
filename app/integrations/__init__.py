391f23ddf2f9a3ed3ba9f60d03413765808ab38a
samuel-rocha-gestao-trafego <samuelrochagestao.trafego@gmail.com>
2026-08-04 11:21:39 -0300

    Add files via upload

---
+++ app/integrations/__init__.py
@@ -0,0 +1,6 @@
+"""
+Integrações futuras do FinanControl.
+
+Este pacote contém serviços de integração com plataformas externas.
+Os ficheiros nesta pasta são esqueletos documentados para implementação futura.
+"""
