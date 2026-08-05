54f186095ac2f05ed5826122c8f1b1fa892873f6
samuel-rocha-gestao-trafego <samuelrochagestao.trafego@gmail.com>
2026-08-05 09:08:19 -0300

    Delete app directory

--- app/integrations/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Integrações futuras do FinanControl.
-
-Este pacote contém serviços de integração com plataformas externas.
-Os ficheiros nesta pasta são esqueletos documentados para implementação futura.
-"""
